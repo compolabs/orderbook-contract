af2b7dd20142911d038898d9facabd85b5716cd7
Braqzen <103777923+Braqzen@users.noreply.github.com>
2024-04-02 14:52:53 +0100

    Account getter tests

--- market-contract/tests/functions/info/account.rs
+++ market-contract/tests/functions/info/account.rs
@@ -2,24 +2,36 @@ mod success {
 
     use crate::utils::{
         interface::{core::deposit, info::account},
-        setup::{create_account, setup},
+        setup::{create_account, setup, Defaults},
     };
 
     #[tokio::test]
     async fn returns_none() {
-        let (contract, owner, _user, _assets) = setup(9, 9, 9).await;
-        assert!(account(&contract, owner).await.value.is_none());
+        let defaults = Defaults::default();
+        let (contract, owner, _user, _assets) = setup(
+            defaults.base_decimals,
+            defaults.quote_decimals,
+            defaults.price_decimals,
+        )
+        .await;
+        assert!(account(&contract, owner.identity()).await.value.is_none());
     }
 
     #[tokio::test]
     async fn returns_account_info() {
-        let (contract, owner, _user, assets) = setup(9, 9, 9).await;
+        let defaults = Defaults::default();
+        let (contract, owner, _user, assets) = setup(
+            defaults.base_decimals,
+            defaults.quote_decimals,
+            defaults.price_decimals,
+        )
+        .await;
         let deposit_amount = 100;
         let expected_account = create_account(deposit_amount, 0, 0, 0);
 
         deposit(&contract, deposit_amount, assets.base.id).await;
 
-        let user_account = account(&contract, owner).await.value.unwrap();
+        let user_account = account(&contract, owner.identity()).await.value.unwrap();
 
         assert_eq!(user_account, expected_account);
     }
