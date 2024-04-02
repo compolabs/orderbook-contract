5531efcdb38a248b5de09cf52c675bea1f46ec74
Braqzen <103777923+Braqzen@users.noreply.github.com>
2024-04-02 14:53:55 +0100

    Fee getter tests

--- market-contract/tests/functions/info/fee.rs
+++ market-contract/tests/functions/info/fee.rs
@@ -1,20 +1,67 @@
 mod success {
 
-    use crate::utils::{interface::info::fee, setup::setup};
-    use fuels::types::Identity;
+    use crate::utils::{
+        interface::{core::set_fee, info::fee},
+        setup::{setup, Defaults},
+    };
 
     #[tokio::test]
     async fn returns_global_fee() {
-        let (contract, owner, _user, assets) = setup(9, 9, 9).await;
+        let defaults = Defaults::default();
+        let (contract, _owner, _user, _assets) = setup(
+            defaults.base_decimals,
+            defaults.quote_decimals,
+            defaults.price_decimals,
+        )
+        .await;
+
+        // Change fee to be non-zero for testing purposes
+        let global_fee = 5;
+        let user = None;
+
+        set_fee(&contract, global_fee, user.clone()).await;
+
+        assert_eq!(fee(&contract, user).await.value, global_fee);
     }
 
     #[tokio::test]
     async fn returns_premium_user_fee() {
-        let (contract, owner, _user, assets) = setup(9, 9, 9).await;
+        let defaults = Defaults::default();
+        let (contract, _owner, user, _assets) = setup(
+            defaults.base_decimals,
+            defaults.quote_decimals,
+            defaults.price_decimals,
+        )
+        .await;
+
+        // Change fee to be non-zero for testing purposes
+        let global_fee = 5;
+        let user_fee = 1;
+
+        set_fee(&contract, global_fee, None).await;
+        set_fee(&contract, user_fee, Some(user.identity())).await;
+
+        assert_eq!(fee(&contract, Some(user.identity())).await.value, user_fee);
     }
 
     #[tokio::test]
     async fn returns_global_fee_for_non_premium_user() {
-        let (contract, owner, _user, assets) = setup(9, 9, 9).await;
+        let defaults = Defaults::default();
+        let (contract, _owner, user, _assets) = setup(
+            defaults.base_decimals,
+            defaults.quote_decimals,
+            defaults.price_decimals,
+        )
+        .await;
+
+        // Change fee to be non-zero for testing purposes
+        let global_fee = 5;
+
+        set_fee(&contract, global_fee, Some(user.identity())).await;
+
+        assert_eq!(
+            fee(&contract, Some(user.identity())).await.value,
+            global_fee
+        );
     }
 }
