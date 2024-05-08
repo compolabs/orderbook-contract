168f8ee8ae7ac123690f52b0237735d43a3a7d53
Paul <zhpv96@gmail.com>
2024-05-08 17:30:25 +0300

    Get order change events script update

--- scripts/get_order_change_events_by_order.rs
+++ scripts/get_order_change_events_by_order.rs
@@ -1,15 +1,15 @@
 use dotenv::dotenv;
 use fuels::{
     prelude::{Provider, WalletUnlocked},
-    types::Bits256,
+    types::{Address, Bits256},
 };
 use orderbook::{
     constants::{ORDERBOOK_CONTRACT_ID, RPC},
-    orderbook_utils::Orderbook,
+    orderbook_utils::{OrderChangeEvent, Orderbook},
     print_title,
 };
 
-const ORDER_ID: &str = "0x8fef66690f7266698f61f77ed151c79614a8daa80525a6f72559acef0829ed90";
+const ORDER_ID: &str = "0x540cbb1379766b2a1f506435d1aecfa71a9cd0bd06522c7a20aabf444487b6b7";
 
 #[tokio::main]
 async fn main() {
@@ -22,11 +22,46 @@ async fn main() {
 
     let orderbook = Orderbook::new(&wallet, ORDERBOOK_CONTRACT_ID).await;
 
-    let res = orderbook
+    match orderbook
         .get_order_change_events_by_order(Bits256::from_hex_str(ORDER_ID).unwrap())
         .await
-        .unwrap()
-        .value;
+    {
+        Ok(result) => {
+            let res = result.value;
+            for event in res {
+                print_order_change_event(event);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
+}
 
-    println!("res = {:?}", res);
+fn print_order_change_event(event: OrderChangeEvent) {
+    let order = event.order.as_ref();
+    let tx_id_hex = format!("0x{}", hex::encode(&event.tx_id.0));
+    println!(
+        "
+        order_id: 0x{:?},
+        sender: {:?},
+        timestamp: {:?},
+        identifier: {:?},
+        tx_id: {:?},
+        base_token: {:?},
+        base_size: {:?},
+        base_price: {:?},
+    ",
+        Address::from(event.order_id.0),
+        event.sender,
+        event.timestamp,
+        event.identifier,
+        tx_id_hex,
+        order.map_or_else(|| "-".to_owned(), |o| o.base_token.to_string()),
+        order.map_or_else(
+            || "-".to_owned(),
+            |o| o.base_size.clone().as_i64().to_string()
+        ),
+        order.map_or_else(|| "-".to_owned(), |o| o.base_price.to_string()),
+    );
 }
