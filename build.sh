c8f0d44c876164366bb2576832dec14f396dc838
alexey <alexnagornnyy.an@gmail.com>
2024-09-26 19:07:25 +0300

    cargo build from root dir

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ forc build --release
 cp -r ./spark-market/out ./spark-market-sdk/spark-market/
 cp -r ./spark-registry/out ./spark-registry-sdk/spark-registry/
 
-cd spark-cli
 cargo clean
 cargo build
-cd ../
